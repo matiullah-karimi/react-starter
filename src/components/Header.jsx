d4271c1454e4e52f138bfd95acee2583e0c37404
Matiullah Karimi <matiullah.karimi@gmail.com>
2021-12-12 15:35:26 +0400

    Fix toggling navigation menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
 export default function Header() {
+    const [showMenu, setShowMenu] = useState(true);
+
     return (
         <header className="bg-gray-800">
             <nav
@@ -26,24 +30,18 @@ export default function Header() {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
+                    onClick={() => setShowMenu(!showMenu)}
                 >
                     <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M4 6h16M4 12h16M4 18h16"
                     />
                 </svg>
 
-                <div className="hidden w-full md:flex md:items-center md:w-auto" id="menu">
-                    <ul
-                        className="
-              pt-4
-              text-base text-gray-200
-              md:flex
-              md:justify-between 
-              md:pt-0"
-                    >
+                <div className={`w-full md:flex md:items-center md:w-auto ${showMenu ? 'block' : ' hidden'}`} id="menu">
+                    <ul className=" pt-4 text-base text-gray-200 md:flex md:justify-between md:pt-0">
                         <li>
                             <a className="md:p-4 py-2 block hover:text-purple-400" href="#"
                             >Features</a
