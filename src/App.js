cc69aedbd3acacbff3616e50a2b6b60a5c7123ea
Matiullah Karimi <matiullah.karimi@gmail.com>
2021-12-12 14:36:35 +0400

    Make app layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./pages/Login";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col bg-gray-50 h-screen">
+      <Header />
+      <main className="flex-1">
+        <Login />
+      </main>
+      <Footer/>
     </div>
   );
 }
