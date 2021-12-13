4802ce6b612b95c3042367c2602fc8c3bca949e8
Matiullah Karimi <matiullah.karimi@gmail.com>
2021-12-13 20:17:58 +0400

    Install react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import Login from "./pages/Login";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from "./pages/Home";
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="flex flex-col bg-gray-50 h-screen">
       <Header />
       <main className="flex-1 max-w-7xl p-4 sm:px-6 md:px-8">
-        <Login />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
       </main>
       <Footer/>
     </div>
