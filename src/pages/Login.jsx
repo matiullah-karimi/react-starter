2f02fe1a0ba2ff0b5692a58222d663d5f8cdb1ba
Matiullah Karimi <matiullah.karimi@gmail.com>
2021-12-14 15:47:05 +0400

    Add login & register state

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,39 @@
+import React from 'react';
 import Input from '../components/Input';
 
-function Login() {
-    return (
-        <div className="flex justify-center items-center h-full">
-            <div className="flex flex-col justify-center items-center w-full sm:w-2/4 md:w-4/12 p-8 bg-white shadow-sm shadow-gray-200 rounded-lg">
-                <span className="text-2xl font-bold mb-8 text-gray-800">Sign in to your account</span>
-                <Input type="email" placeholder="Email"/>
-                <Input type="password" placeholder="Password"/>
-                <a href="#" className="font-semibold text-gray-700 hover:text-gray-900 my-4 self-end">Forgot Passwod</a>
-                <a className="btn btn-primary w-full" href="/login">Login</a>
+class Login extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit = () => {
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div className="flex justify-center items-center h-full">
+                <div className="flex flex-col justify-center items-center w-full sm:w-2/4 md:w-4/12 p-8 bg-white shadow-sm shadow-gray-200 rounded-lg">
+                    <span className="text-2xl font-bold mb-8 text-gray-800">Sign in to your account</span>
+                    <Input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange}/>
+                    <Input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
+                    <a href="#" className="font-semibold text-gray-700 hover:text-gray-900 my-4 self-end">Forgot Passwod</a>
+                    <button className="btn btn-primary w-full" href="/login" onClick={this.handleSubmit}>Login</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,42 @@
+import React from 'react';
 import Input from '../components/Input';
 
-function Register() {
-    return (
-        <div className="flex justify-center items-center h-full">
-            <div className="flex flex-col justify-center items-center w-full sm:w-2/4 md:w-4/12 p-8 bg-white shadow-sm shadow-gray-200 rounded-lg">
-                <span className="text-2xl font-bold mb-8 text-gray-800">Sign up</span>
-                <Input type="text" placeholder="Name"/>
-                <Input type="email" placeholder="Email"/>
-                <Input type="password" placeholder="Password"/>
-                <a className="btn btn-primary w-full mt-4" href="/login">Register</a>
+class Register extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            password_confirmation: ''
+        }
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit = () => {
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div className="flex justify-center items-center h-full">
+                <div className="flex flex-col justify-center items-center w-full sm:w-2/4 md:w-4/12 p-8 bg-white shadow-sm shadow-gray-200 rounded-lg">
+                    <span className="text-2xl font-bold mb-8 text-gray-800">Sign up</span>
+                    <Input type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange}/>
+                    <Input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange}/>
+                    <Input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
+                    <Input type="password" name="password_confirmation" placeholder="Confirm Password" value={this.state.password_confirmation} onChange={this.handleChange}/>
+                    <button className="btn btn-primary w-full mt-4" onClick={this.handleSubmit}>Register</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Register;
