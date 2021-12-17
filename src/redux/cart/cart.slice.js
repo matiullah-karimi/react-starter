43b6348457ba2bdb33e3815974fdb8646bb92623
Matiullah Karimi <matiullah.karimi@gmail.com>
2021-12-17 08:13:15 +0400

    add remove from cart functionality

--- src/redux/cart/cart.slice.js
+++ src/redux/cart/cart.slice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   items: [],
+  hidden: true
 }
 
 export const cartSlice = createSlice({
@@ -19,13 +20,28 @@ export const cartSlice = createSlice({
 
       state.items.push({...action.payload, quantity: 1});
     },
+
+    removeFromCart: (state, action) => {
+      const itemIndex = state.items.findIndex(item => item.id === action.payload.id);
+
+      if (itemIndex > -1) {
+        state.items.splice(itemIndex, 1);
+      }
+    },
+
+    toggleCartDropdownHidden(state) {
+      state.hidden = !state.hidden;
+    }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { addToCart } = cartSlice.actions
+export const { addToCart, removeFromCart, toggleCartDropdownHidden } = cartSlice.actions
 
 export default cartSlice.reducer
 
 // Selectors
 export const selectCartItemsQuantity = state => state.cart.items.reduce((a, b) => a + b.quantity, 0);
+export const selectCartItemsPrice = state => state.cart.items.reduce((a, b) => a + (b.quantity * b.price), 0).toFixed(2);
+export const selectCartItems = state => state.cart.items;
+export const selectCartDropdownHidden = state => state.cart.hidden;
