d9144e391f33f68263fc3bd121e8675558554eb7
Matiullah Karimi <matiullah.karimi@gmail.com>
2021-12-18 18:53:41 +0400

    add lable for input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,16 @@
 const Input = props => (
-    <input 
-        className={`${props.className} py-3 px-4 border border-gray-200 m-2 rounded-md w-full outline-gray-300`}
-        type={props.type}
-        name={props.name}
-        value={props.value}
-        onChange={props.onChange}
-        placeholder={props.placeholder} />
+    <div className="flex flex-col w-full">
+        {
+            props.label && <label className={`text-gray-600 text-sm font-semibold mb-1 ${props.labelClassName}`}>{props.label}</label>
+        }
+        <input 
+            className={`${props.className} py-3 px-4 border border-gray-200 rounded-md w-full outline-gray-300`}
+            type={props.type}
+            name={props.name}
+            value={props.value}
+            onChange={props.onChange}
+            placeholder={props.placeholder} />
+    </div>
 );
 
 export default Input;
