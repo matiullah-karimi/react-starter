c9310def9945d5dfcc15ce7686a16128fb834f79
Matiullah Karimi <matiullah.karimi@gmail.com>
2021-12-18 20:13:59 +0400

    Fix input spacing issue

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -49,9 +49,13 @@ const Login = () => {
         <div className="flex justify-center items-center h-full">
             <div className="flex flex-col justify-center items-center w-full sm:w-2/4 md:w-4/12 p-8 bg-white shadow-sm shadow-gray-200 rounded-lg">
                 <span className="text-2xl font-bold mb-8 text-gray-800">Sign in to your account</span>
-                <Input type="email" name="email" placeholder="Email" value={state.email} onChange={handleChange} />
-                <Input type="password" name="password" placeholder="Password" value={state.password} onChange={handleChange} />
-                <a href="#" className="font-semibold text-gray-700 hover:text-gray-900 my-4 self-end">Forgot Passwod</a>
+                <div className='mt-4 w-full'>
+                    <Input type="email" name="email" placeholder="Email" value={state.email} onChange={handleChange} />
+                </div>
+                <div className='mt-4 w-full'>
+                    <Input type="password" name="password" placeholder="Password" value={state.password} onChange={handleChange} />
+                </div>
+                <a href="#" className="font-semibold text-sm text-gray-700 hover:text-gray-900 my-4 self-end">Forgot Passwod</a>
                 <LoadingButton loading={state.loading} onClick={handleSubmit}> Login </LoadingButton>
             </div>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -28,10 +28,21 @@ class Register extends React.Component {
             <div className="flex justify-center items-center h-full">
                 <div className="flex flex-col justify-center items-center w-full sm:w-2/4 md:w-4/12 p-8 bg-white shadow-sm shadow-gray-200 rounded-lg">
                     <span className="text-2xl font-bold mb-8 text-gray-800">Sign up</span>
-                    <Input type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange}/>
-                    <Input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange}/>
-                    <Input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
-                    <Input type="password" name="password_confirmation" placeholder="Confirm Password" value={this.state.password_confirmation} onChange={this.handleChange}/>
+                    <div className="mt-4 w-full">
+                        <Input type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange}/>
+                    </div>
+
+                    <div className='mt-4 w-full'>
+                        <Input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange}/>
+                    </div>
+
+                    <div className="mt-4 w-full">
+                        <Input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
+                    </div>
+
+                    <div className="mt-4 w-full">
+                        <Input type="password" name="password_confirmation" placeholder="Confirm Password" value={this.state.password_confirmation} onChange={this.handleChange}/>
+                    </div>
                     <button className="btn btn-primary w-full mt-4" onClick={this.handleSubmit}>Register</button>
                 </div>
             </div>
